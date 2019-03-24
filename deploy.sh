1b11c0ac4929dbac7bb792978e68d08e34f0f187
Krystian Sitarek <krystiansitarek@gmail.com>
2019-03-24 15:20:53 +0100

    Fix for deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_BRANCH" -eq "master" ]
+if [ "$TRAVIS_BRANCH" = "master" ]
 then
     echo "Deploy to production"
     find dist -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASSWORD ftp://$FTP_HOST/{} \\;
-elif [ "$TRAVIS_BRANCH" -eq "develop" ]
+elif [ "$TRAVIS_BRANCH" = "develop" ]
 then
     echo "Deploy to develop"
     find dist -type f -exec curl --ftp-create-dirs -T {} -u $FTP_DEVELOP_USER:$FTP_DEVELOP_PASSWORD ftp://$FTP_HOST/{} \\;
