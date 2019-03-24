12dcf18d54dd09b399611dbbfc721459d0f4e1d5
Krystian Sitarek <krystiansitarek@gmail.com>
2019-03-24 13:22:41 +0100

    Added placeholder components for programming, devops and contact pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WhoamiComponent } from './whoami/whoami.component';
 import { NotFoundComponent } from './notfound/notfound.component';
+import { ProgrammingComponent } from './programming/programming.component';
+import { DevopsComponent } from './devops/devops.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: WhoamiComponent },
+  { path: 'programming', component: ProgrammingComponent },
+  { path: 'devops', component: DevopsComponent },
+  { path: 'contact', component: ContactComponent },
 
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '/404' }
